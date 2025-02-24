ad2c559ba2a9e214ae01adefb88b97527bccf5da
benjamin fejzagic <benjamin.fejzagic@hotmail.com>
2025-02-24 15:00:34 +0100

    work in progres resources

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,5 +2,5 @@ class Activity < ApplicationRecord
   belongs_to :task
   belongs_to :activityable, polymorphic: true
 
-  enum :activity_type, { worker: 0, machine: 1, material: 2 }
+  enum :activity_type, { worker: 0, machine: 1, material: 2, custom: 3 }
 end
