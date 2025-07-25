d3fd7d47b5cd336e009613312f09bac2404aff3c
benjamin fejzagic <benjamin.fejzagic@hotmail.com>
2025-07-25 23:11:18 +0200

    Service calculation in progress

--- app/controllers/pinned_norms_controller.rb
+++ app/controllers/pinned_norms_controller.rb
@@ -2,18 +2,32 @@ class PinnedNormsController < ApplicationController
   before_action :set_sub_task
 
   def create
-    @sub_task.pinned_norms << Norm.find(params[:norm_id]) unless @sub_task.pinned_norms.exists?(params[:norm_id])
-    head :ok
+    norm = Norm.find(params[:norm_id])
+    if !@sub_task.pinned_norms.exists?(norm.id)
+      @sub_task.pinned_norms << norm
+      SubTaskPlanningCalculator.new(@sub_task).call(norms: true)
+    end
+
+    render json: sub_task_response_data
   end
 
   def destroy
-    @sub_task.pinned_norms.destroy(Norm.find(params[:norm_id]))
-    head :ok
+    norm = Norm.find(params[:norm_id])
+    if @sub_task.pinned_norms.exists?(norm.id)
+      @sub_task.pinned_norms.destroy(norm)
+      SubTaskPlanningCalculator.new(@sub_task).call(norms: true)
+    end
+
+    render json: sub_task_response_data
   end
 
   private
 
   def set_sub_task
     @sub_task = SubTask.find(params[:sub_task_id])
+  end
+
+  def sub_task_response_data
+    @sub_task.slice(:duration, :num_workers_skilled, :num_workers_unskilled, :num_machines).merge(success: true)
   end
 end
--- app/javascript/controllers/norms_controller.js
+++ app/javascript/controllers/norms_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static values = { subtaskId: Number }
+  static targets = ["duration", "skilled", "unskilled", "machines"]
 
   trackSelection(event) {
     const checkbox = event.target
@@ -16,9 +17,26 @@ export default class extends Controller {
       method: method,
       headers: {
         "X-CSRF-Token": token,
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Accept": "application/json"
       }
     })
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        this.durationTarget.value = data.duration
+        this.skilledTarget.value = data.num_workers_skilled
+        this.unskilledTarget.value = data.num_workers_unskilled
+        this.machinesTarget.value = data.num_machines
+
+        // 🔁 Ručno pokreni recalculaciju u subtask kontroleru
+        const subtaskController = this.application.getControllerForElementAndIdentifier(this.element, "subtask")
+        if (subtaskController) {
+          subtaskController.recalculate()
+        }
+      }
+    })
+    .catch(error => console.error("Error updating norms:", error))
   }
 
   getBusinessId() {
