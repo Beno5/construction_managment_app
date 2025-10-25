a1fe07d302190418675be604cb2662ecac586f6a
benjamin fejzagic <benjamin.fejzagic@hotmail.com>
2025-10-25 22:54:03 +0200

    norms mapping text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def t_enum(record, enum_attr)
+    return "-" if record.send(enum_attr).blank?
+
+    I18n.t("activerecord.attributes.#{record.model_name.i18n_key}.#{enum_attr}.#{record.send(enum_attr)}")
+  end
 end
