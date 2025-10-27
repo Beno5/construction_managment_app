bfdba448c8dd5ac29e6e225638c1aae040d60ad7
benjamin fejzagic <benjamin.fejzagic@hotmail.com>
2025-10-27 15:37:10 +0100

    mailing done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    passwords: 'users/passwords'
+  }
   root to: "businesses#index"
 
   # Business routes
