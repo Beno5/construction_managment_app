24cf4efa9153a4a86a3008d9c941caf5cb4f8677
benjamin fejzagic <benjamin.fejzagic@hotmail.com>
2025-11-13 21:16:45 +0100

    Fix Redis env var detection for Heroku

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV.fetch("REDIS_URL") }
-  end
+redis_url = ENV["REDIS_URL"] || ENV["REDISCLOUD_URL"] || "redis://localhost:6379"
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV.fetch("REDIS_URL") }
-  end
+Sidekiq.configure_server do |config|
+  config.redis = { url: redis_url }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: redis_url }
 end
